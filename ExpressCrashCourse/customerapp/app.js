0df3f9366317b33507d287d1c04b5fe2c8346746
Maheedhar Gunnam <maheedhargunnam@gmail.com>
2018-01-29 06:44:50 -0500

    changes to app.js in the building process

--- ExpressCrashCourse/customerapp/app.js
+++ ExpressCrashCourse/customerapp/app.js
@@ -1,1 +1,7 @@
-console.log("Hello World");
+const express = require('express');
+const bodyparser = require('body-parser');
+
+var app = express();
+app.listen(3000,function(){
+	console.log("Server started on port 3000");
+});
